9c7d954f28a22140af1524731fe1e36d2f18a0bb
Nilaf Talapady <nilaf@moonraft.ocm>
2014-03-19 23:24:37 +0530

    Added config and upload methods

--- src/ios/AS3Uploader.h
+++ src/ios/AS3Uploader.h
@@ -1,20 +1,69 @@
 //
-//  AS3Uploader.h
+//  AS3Uploader.m
 //  AS3Uploader
 //
 //  Created by Nilaf Talapady on 19/03/14.
 //  Copyright (c) 2014 Nilaf Talapady. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-#import <Cordova/CDV.h>
+#import "AS3Uploader.h"
 
-#import "AFAmazonS3Manager.h"
+@implementation AS3Uploader
+
+//Cordova
 
 
-@interface AS3Uploader : CDVPlugin{
-    CDVInvokedUrlCommand *_command;
+-(void)uploadFile:(NSString*)path{
+    AFAmazonS3Manager *s3Client = [[AFAmazonS3Manager alloc] initWithAccessKeyID:accessId secret:secretKey];
+    s3Client.requestSerializer.bucket = bucket;
+
+    s3Client.requestSerializer.timeoutInterval = 240;
+    [s3Client postObjectWithFile:path
+                 destinationPath:@"/"
+                      parameters:nil
+                        progress:^(NSUInteger bytesWritten, NSInteger totalBytesWritten, NSInteger totalBytesExpectedToWrite) {
+                            double progress = (totalBytesWritten / (totalBytesExpectedToWrite * 1.0f) * 100);
+                            CDVPluginResult *pluginResult =  [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDouble:progress];
+                            [self.commandDelegate sendPluginResult:pluginResult callbackId:_command.callbackId];
+                        }
+                         success:^(id responseObject) {
+                             CDVPluginResult *pluginResult =  [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Success"];
+                             pluginResult.setKeepCallbackAsBool = false;
+                             [self.commandDelegate sendPluginResult:pluginResult callbackId:_command.callbackId];
+
+                         }
+                         failure:^(NSError *error) {
+                             NSLog(@"Error: %@", error);
+                         }];
 }
 
-- (void)stopRecording:(CDVInvokedUrlCommand*)command;
+- (void)config:(CDVInvokedUrlCommand*)command
+{
+        
+    [self.commandDelegate runInBackground:^{
+
+        accessId = [command.arguments objectAtIndex:0];
+        secretKey = [command.arguments objectAtIndex:1];
+        bucket = [command.arguments objectAtIndex:2];
+        CDVPluginResult *pluginResult =  [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:_command.callbackId];
+        
+    }];
+}
+- (void)upload:(CDVInvokedUrlCommand*)command
+{
+    
+    [self.commandDelegate runInBackground:^{
+        _command = command
+        
+        url = [command.arguments objectAtIndex:0];
+        [self uploadFile:url];
+        CDVPluginResult *pluginResult =  [CDVPluginResult resultWithStatus:CDVCommandStatus_NO_RESULT];
+        pluginResult.setKeepCallbackAsBool = true;
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:_command.callbackId];
+        
+    }];
+}
+
+
 @end
--- src/ios/AS3Uploader.m
+++ src/ios/AS3Uploader.m
@@ -12,11 +12,54 @@
 
 //Cordova
 
-- (void)stopRecording:(CDVInvokedUrlCommand*)cmd
+
+-(void)uploadFile:(NSString*)path{
+    AFAmazonS3Manager *s3Client = [[AFAmazonS3Manager alloc] initWithAccessKeyID:accessId secret:secretKey];
+    s3Client.requestSerializer.bucket = bucket;
+
+    s3Client.requestSerializer.timeoutInterval = 240;
+    [s3Client postObjectWithFile:path
+                 destinationPath:@"/"
+                      parameters:nil
+                        progress:^(NSUInteger bytesWritten, NSInteger totalBytesWritten, NSInteger totalBytesExpectedToWrite) {
+                            double progress = (totalBytesWritten / (totalBytesExpectedToWrite * 1.0f) * 100);
+                            CDVPluginResult *pluginResult =  [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDouble:progress];
+                            [self.commandDelegate sendPluginResult:pluginResult callbackId:_command.callbackId];
+                        }
+                         success:^(id responseObject) {
+                             CDVPluginResult *pluginResult =  [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Success"];
+                             pluginResult.setKeepCallbackAsBool = false;
+                             [self.commandDelegate sendPluginResult:pluginResult callbackId:_command.callbackId];
+
+                         }
+                         failure:^(NSError *error) {
+                             NSLog(@"Error: %@", error);
+                         }];
+}
+
+- (void)config:(CDVInvokedUrlCommand*)command
 {
+        
     [self.commandDelegate runInBackground:^{
-        _command = cmd;
+
+        accessId = [command.arguments objectAtIndex:0];
+        secretKey = [command.arguments objectAtIndex:1];
+        bucket = [command.arguments objectAtIndex:2];
+        CDVPluginResult *pluginResult =  [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:_command.callbackId];
+        
+    }];
+}
+- (void)upload:(CDVInvokedUrlCommand*)command
+{
+    
+    [self.commandDelegate runInBackground:^{
+        _command = command
+        
+        url = [command.arguments objectAtIndex:0];
+        [self uploadFile:url];
         CDVPluginResult *pluginResult =  [CDVPluginResult resultWithStatus:CDVCommandStatus_NO_RESULT];
+        pluginResult.setKeepCallbackAsBool = true;
         [self.commandDelegate sendPluginResult:pluginResult callbackId:_command.callbackId];
         
     }];
